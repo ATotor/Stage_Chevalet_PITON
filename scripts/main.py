24a410bb0788d8320bd1cff18f9632f61a3002a0
Totor <pitonlndb@gmail.com>
2024-03-01 17:31:45 +0100

    UK-simulation F-terms correction

--- scripts/main.py
+++ scripts/main.py
@@ -1,29 +1,49 @@
 # -*- coding: utf-8 -*-
 
-
-import src.UK as UK
 import numpy as np
-import scipy as sp
+import argparse
+import sys
 import matplotlib.pyplot as plt
 
-M, K, C, W, V, phi, Fext, q0, qd0 ,qdd0, h = UK.ANTUNES_2017()
+import src.UK as UK
+import src.disp as disp
+
+print('')
+
+# Getting the user's input
+if __name__ == "__main__":
+     parser = argparse.ArgumentParser(description="Run a specific function.")
+     parser.add_argument("--uncoupled", action = "store_true", 
+                         help='Whether the string and body of the instrument' +
+                         'are uncoupled')
+
+     args = parser.parse_args()
+     coupled = not args.uncoupled
+
+# Getting system's model and initial variables 
+M, K, C, W, V, Wc, Vc, phi, phi_c, Fext, q, qd ,qdd, Fc, h = \
+    UK.ANTUNES_2017(coupled)
 
 Nt, Nn  = Fext.shape
+Nc      = phi_c.shape[0]
+Nx      = phi.shape[0]
 
-q, qd, qdd  = np.zeros((Nt, Nn)), np.zeros((Nt, Nn)), \
-            np.zeros((Nt, Nn))
-q[0], qd[0], qdd[0] = q0, qd0, qdd0
+# Data arrays' initialization
+x, xd, xdd, Fc_phys             = np.zeros((Nt, Nx)), np.zeros((Nt, Nx)), \
+                                    np.zeros((Nt, Nx)), np.zeros((Nt, Nc))
+x[0], xd[0], xdd[0], Fc_phys[0] = UK.give_phys(phi, phi_c, q, qd, qdd, Fc)
 
+# Main simulation loop
 print('------- Simulation running -------')
 for i in range(1,Nt):
-    if not (100*i/Nt)%5:
-        print(f'{100*i//Nt} %')
-    q[i], qd[i], qdd[i] = UK.UK_step(M, K, C, Fext[i], W, V, q[i-1], 
-                                  qd[i-1], qdd[i-1], h)
-print('------- Simulation over -------')
+    if (not (100*i/Nt)%5) or i==1 :
+        sys.stdout.write(f"\rProgression : {100*i//Nt} %")
+        sys.stdout.flush()
+    q, qd, qdd, Fc                  = UK.UK_step(M, K, C, Fext[i], W, V, Wc, \
+                                              Vc, q, qd, qdd, h)
+    x[i], xd[i], xdd[i], Fc_phys[i] = UK.give_phys(phi, phi_c, q, qd, qdd, Fc)
+print('\n------- Simulation over -------')
 
-x, xd, xdd = UK.give_x(phi, q, qd, qdd)
-
-for i in range(x.shape[-1]):
-    plt.figure(figsize=(15,5))
-    plt.plot(x[:,0])
+# Results display
+disp.set_gui_qt()
+disp.summary_plot(x, Fc_phys, h)
--- src/UK.py
+++ src/UK.py
@@ -4,22 +4,39 @@ import numpy as np
 import scipy as sp
 
 
-# Inputs :  M           : (Nn,Nn)       modal mass matrix
-#           K           : (Nn,Nn)       modal rigidity matrix
-#           C           : (Nn,Nn)       modal damping matrix
-#           Fext        : (Nn)          external modal force vector
-#           W           : (Nn,Nn)       modal W matrix (such that 
-#                                       qdd = W @ qudd + V)
-#           V           : (Nn)          modal V matrix (such that 
-#                                       qdd = W @ qudd + V)
-#           q_prev      : (Nn)          previous modal response 
-#           qd_prev     : (Nn)          previous modal response's velocity
-#           qdd_prev    : (Nn)          previous modal response's acceleration
-#           h           : ()            simulation time step
-# Outputs : q0          : (Nn)          current modal response 
-#           qd0         : (Nn)          current modal response's velocity
-#           qdd0        : (Nn)          current modal response's acceleration
-def UK_step(M, K, C, Fext, W, V, q_prev, qd_prev, qdd_prev, h):
+
+'''
+-------------------------------------------------------------
+                   UK-SIMULATION FUNCTIONS
+-------------------------------------------------------------
+'''
+
+'''
+Description :   make a UK-step for a given UK model, with given modal states 
+                and force.
+ 
+Inputs :  M           : (Nn,Nn)       modal mass matrix
+          K           : (Nn,Nn)       modal rigidity matrix
+          C           : (Nn,Nn)       modal damping matrix
+          Fext        : (Nn)          external modal force vector
+          W           : (Nn,Nn)       modal W matrix (such that 
+                                      qdd = W @ qudd + V)
+          V           : (Nn)          modal V matrix (such that 
+                                      qdd = W @ qudd + V)
+          Wc          : (Nn,Nn)       modal Wc matrix (such that 
+                                      Fc = Wc @ qudd + Vc)
+          Vc          : (Nn)          modal V matrix (such that 
+                                      Fc = Wc @ qudd + Vc)
+          q_prev      : (Nn)          previous modal response 
+          qd_prev     : (Nn)          previous modal response's velocity
+          qdd_prev    : (Nn)          previous modal response's acceleration
+          h           : ()            simulation time step
+Outputs : q0          : (Nn)          current modal response 
+          qd0         : (Nn)          current modal response's velocity
+          qdd0        : (Nn)          current modal response's acceleration
+          Fc          : (Nt,Nn)       constraint modal force matrix
+'''
+def UK_step(M, K, C, Fext, W, V, Wc, Vc, q_prev, qd_prev, qdd_prev, h):
     
     # Computation of the current modal response and derivative approximates 
     # thanks to the previous state
@@ -28,35 +45,77 @@ def UK_step(M, K, C, Fext, W, V, q_prev, qd_prev, qdd_prev, h):
     
     # Computation of the current modal acceleration thanks to the UK
     # formulation
-    F       = Fext - C@qd_mid - K@q
-    qudd    = M.power(-1)@F
+    F       = Fext - C @ qd_mid - K @ q
+    F       = F[0,:]        # Flatten the F array (due to sp/np compatibility)
+    qudd    = M.power(-1) @ F
     qdd     = W @ qudd + V
     qd      = qd_prev + 0.5*h*(qdd_prev + qdd)
+    Fc      = Wc @ qudd + Vc
     
-    return q, qd, qdd
+    return q, qd, qdd, Fc
+
+'''
+Description :   gives the physical state x, xd, xdd and constraint force 
+                Fc_phys for the given modal state q, qd, qdd and modal 
+                constraint force Fc.
+
+Inputs :  phi         : (Nx,Nn)       string's modeshapes  
+          phi_c       : (Nm,Nn)       system's modeshapes at constraints' 
+                                      locations
+          q           : ([Nt],Nn)     modal response 
+          qd          : ([Nt],Nn)     modal response's velocity
+          qdd         : ([Nt],Nn)     modal response's acceleration
+          Fc          : ((Nt],Nn)     constraint modal force matrix
+Outputs : x           : ([Nt],Nx)     physical response
+          xd          : ([Nt],Nx)     physical response's velocity
+          xdd         : ([Nt],Nx)     physical response's acceleration
+          Fc_phys     : ([Nt],Nc)     constraint force matrix
+'''
+def give_phys(phi, phi_c, q, qd, qdd, Fc):
+    x       = q @ phi.T
+    xd      = qd @ phi.T
+    xdd     = qdd @ phi.T
+    Fc_phys = Fc @ give_MP_inv(phi_c)
+    
+    return x, xd, xdd, Fc_phys
 
 
-# Inputs :  A           : (Nm, Nn)      modal constraint matrix
-#           M           : (Nn,Nn)       modal mass matrix
-#           b           : (Nm)          modal constraint vector
-# Outputs : W           : (Nn,Nn)       modal W matrix (such that 
-#                                       qdd = W @ qudd + V)
-#           V           : (Nn)          modal V matrix (such that 
-#                                       qdd = W @ qudd + V)
+'''
+Description :   gives the Moore-Penrose right pseudoinverse of a given matrix
+                M.
+
+Inputs : M  : 2d numpy array
+'''
+def give_MP_inv(M):
+    return M.T @ np.linalg.inv(M @ M.T)
+
+
+'''
+Description :   gives the W and V matrices from the UK modal formalism for a 
+                given model. W and V are such that qdd = W @ qudd + V.
+    
+Inputs :  A           : ([Na],Nm, Nn) modal constraint matrix
+          M           : (Nn,Nn)       modal mass matrix
+          b           : ([Na],Nm)     modal constraint vector
+Outputs : W           : ([Na],Nn,Nn)  modal W matrix (such that 
+                                      qdd = W @ qudd + V)
+          V           : ([Na],Nn)     modal V matrix (such that 
+                                      qdd = W @ qudd + V)
+'''
 def give_W_V(A, M, b):
     # Case where Bp is computed off-line
     if len(A.shape)>2:
-        Nc,Nm,Nn = A.shape
+        Na,Nm,Nn = A.shape
         I           = np.eye(Nn)
         M_sq_inv    = M.power(-1/2)
-        W           = np.zeros((Nc, Nn, Nn))
+        W           = np.zeros((Na, Nn, Nn))
         V           = np.zeros((Nn,1))
-        for i in range(Nc):
+        for i in range(Na):
             B       = A[i] @ M_sq_inv
-            Bp      = B.T @ np.linalg.inv(B @ B.T)
+            Bp      = give_MP_inv(B)
             MB      = M_sq_inv @ Bp
             W[i]    = I - MB @ A[i]
-            V[i]    = MB @ b
+            V[i]    = MB @ b[i]
     # Case where Bp is computed on-line
     else:
         M_sq_inv    = M.power(-1/2)
@@ -69,27 +128,52 @@ def give_W_V(A, M, b):
     return W, V
 
 
-# Inputs  : Fext_phys   : (Nx)         external force vector 
-#           phi         : (Nx,Nn)       modal deformation vector
-# Outputs : Fext        : (Nn)          modal external force vector
+'''
+Description :   gives UK's model external physical force matrix Fext_phys for 
+                a given modal force matrix Fext and modal deformation matrix.
+
+Inputs  : Fext_phys   : ([Nt],Nx)     external force vector 
+          phi         : (Nx,Nn)       modal deformation matrix
+Outputs : Fext        : ([Nt],Nn)     modal external force vector
+'''
 def give_Fext(Fext_phys, phi):
-    return Fext_phys @ phi
+    return sp.sparse.csr_array(Fext_phys @ phi)
 
+'''
+-------------------------------------------------------------
+                   UK-MODEL FUNCTIONS
+-------------------------------------------------------------
+'''
 
-# Outputs : M           : (Nn,Nn)       modal mass matrix
-#           K           : (Nn,Nn)       modal rigidity matrix
-#           C           : (Nn,Nn)       modal damping matrix
-#           W           : (Nn,Nn)       modal W matrix (such that 
-#                                       qdd = W @ qudd + V)
-#           V           : (Nn)          modal V matrix (such that 
-#                                       qdd = W @ qudd + V)
-#           phi         : (Nx,Nn)      string's modeshapes  
-#           Fext        : (Nt,Nn)       external modal force matrix
-#           q0          : (Nn)          intial modal response 
-#           qd0         : (Nn)          intial modal response's velocity
-#           qdd0        : (Nn)          intial modal response's acceleration
-#           h           : ()            simulation time step
-def ANTUNES_2017():
+'''
+Description :   gives the UK modal model of a one-string guitare. This model 
+                is from J.ANTUNES and V.DEBUT, 2017.
+
+Outputs : M           : (Nn,Nn)       modal mass matrix
+          K           : (Nn,Nn)       modal rigidity matrix
+          C           : (Nn,Nn)       modal damping matrix
+          W           : (Nn,Nn)       modal W matrix (such that 
+                                      qdd = W @ qudd + V)
+          V           : (Nn)          modal V matrix (such that 
+                                      qdd = W @ qudd + V)
+          Wc          : (Nn,Nn)       modal Wc matrix (such that 
+                                      Fc = Wc @ qudd + Vc)
+          Vc          : (Nn)          modal V matrix (such that 
+                                      Fc = Wc @ qudd + Vc)
+          phi         : (Nx,Nn)       string's modeshapes  
+          phi_c       : (Nm,Nn)       system's modeshapes at constraints' 
+          Fext        : (Nt,Nn)       external modal force matrix
+          q0          : (Nn)          intial modal response 
+          qd0         : (Nn)          intial modal response's velocity
+          qdd0        : (Nn)          intial modal response's acceleration
+          Fc          : (Nn)          initial constraint modal force matrix
+          h           : ()            simulation time step
+'''
+def ANTUNES_2017(coupled=True):
+    if coupled:
+        print('Model used : ANTUNES_2017 coupled')
+    else:
+        print('Model used : ANTUNES_2017 uncoupled')
     h       = 1e-5
     t       = np.arange(0,10, h) 
     tE      = 1e-2
@@ -124,11 +208,11 @@ def ANTUNES_2017():
     for i in range(n.size):
         phi_s[:,i] = np.sin(p[i]*x)
     
-    Fext_phys_s             =  np.zeros((Nt, Nx))
+    # Fext_phys_s             =  np.zeros((Nt, Nx))  # CHANGE TO SCIPY ARRAY !!!!!!!!!!!!!!!!!!!
+    Fext_phys_s             = sp.sparse.csr_array((Nt,Nx))
     Fext_phys_s[t<tE,idxE]  =  5*t[t<tE]/t[t<tE][-1]
-    Fext_s                  = np.zeros((Nt, Nn_s))
-    for i in range(Nn_s):
-        Fext_s[i] = give_Fext(Fext_phys_s[i], phi_s)  # BUG HERE !!!!!!!!!!!!!!!!!
+    Fext_s                  = give_Fext(Fext_phys_s, phi_s) 
+    
     
     m_s = rho_l*L/2*np.ones(Nn_s)
     
@@ -160,7 +244,8 @@ def ANTUNES_2017():
     
     c_b = 2*m_b*w_b*zeta_b
     
-    Fext_b  = np.zeros((Nt,Nn_b))
+    #Fext_b  = np.zeros((Nt,Nn_b)) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    Fext_b   = sp.sparse.csr_array((Nt,Nn_b))
     
     # Overall model
     
@@ -172,102 +257,93 @@ def ANTUNES_2017():
     K   = sp.sparse.dia_array(np.diag(k))
     C   = sp.sparse.dia_array(np.diag(c))
     
-    Fext = np.concatenate((Fext_s,Fext_b), axis=-1)
-    
     Nn = m.shape[0]
     
-    phi = np.zeros((Nx + 1, Nn))
+    #Fext = np.concatenate((Fext_s,Fext_b), axis=-1)
+    Fext = sp.sparse.hstack([Fext_s, Fext_b])
+    Fext = sp.sparse.csr_array(Fext)
+    
+    phi             = np.zeros((Nx + 1, Nn))
     phi[:Nx, :Nn_s] = phi_s
     phi[Nx:, Nn_s:] = phi_b
+    
+    phi_c           = phi[[0,2]]
     
     # Constraints
     
     A = np.zeros((2,Nn))
     A[0,:Nn_s] = phi_s[-1]
     A[1,:Nn_s] = phi_s[idxF]
+    if coupled:
+        A[0, Nn_s:] = -phi_b[0].T
     
     b = np.zeros((2)) 
     
     # Bp matrix
     
+    M_sq        = M.power(1/2)
     M_sq_inv    = M.power(-1/2)
     B           = A @ M_sq_inv
     Bp          = B.T @ np.linalg.inv((B @ B.T))
     MB          = M_sq_inv @ Bp
     W           = np.eye(Nn) - MB @ A
     V           = MB @ b 
+    Wc          = -M_sq @ Bp @ A
+    Vc          = M_sq @ Bp @ b
     
     # Initial conditions
     
-    q0      = np.zeros(M.shape[0])
-    qd0     = np.zeros(M.shape[0])
-    qdd0    = np.zeros(M.shape[0]) 
+    q0      = np.zeros(Nn)
+    qd0     = np.zeros(Nn)
+    qdd0    = np.zeros(Nn) 
+    Fc0     = np.zeros(Nn)
     
-    return M, K, C, W, V, phi, Fext, q0, qd0 ,qdd0, h
-
-
-# Inputs :  phi         : (Nx,Nn)       string's modeshapes  
-#           q           : (Nt,Nn)       modal response 
-#           qd          : (Nt,Nn)       modal response's velocity
-#           qdd         : (Nt,Nn)       modal response's acceleration
-# Outputs : x           : (Nt,Nx)       physical response
-#           xd          : (Nt,Nx)       physical response's velocity
-#           xdd         : (Nt,Nx)       physical response's acceleration
-def give_x(phi, q, qd, qdd):
-    return q @ phi.T, qd @ phi.T, qdd @ phi.T
+    return M, K, C, W, V, Wc, Vc, phi, phi_c, Fext, q0, qd0 ,qdd0, Fc0, h
     
 
-# def main() -> None:
-#     import matplotlib.pyplot as plt
-    
-#     M, K, C, W, V, phi, Fext, q0, qd0 ,qdd0, h = ANTUNES_2017()
-    
-#     Nt, Nn  = Fext.shape
-    
-#     q, qd, qdd  = np.zeros((Nt, Nn)), np.zeros((Nt, Nn)), \
-#                 np.zeros((Nt, Nn))
-#     q[0], qd[0], qdd[0] = q0, qd0, qdd0
-    
-#     print('------- Simulation running -------')
-#     for i in range(1,Nt):
-#         if not (100*i/Nt)%5:
-#             print(f'{100*i//Nt} %')
-#         q[i], qd[i], qdd[i] = UK_step(M, K, C, Fext[i], W, V, q[i-1], 
-#                                       qd[i-1], qdd[i-1], h)
-#     print('------- Simulation over -------')
-    
-#     x, xd, xdd = give_x(phi, q, qd, qdd)
-    
-#     #plt.plot(x[:,0])
-#     #plt.plot(x[:,1])
-#     #plt.plot(x[:,2])
-#     print('hi')
-    
-#     return
-    
 
-# if __name__ == "__main__":
-#     main()
+'''
+-------------------------------------------------------------
+                   MAIN FUNCTION
+-------------------------------------------------------------
+'''
+
+def main() -> None:
     
-import matplotlib.pyplot as plt
+    import disp
 
-M, K, C, W, V, phi, Fext, q0, qd0 ,qdd0, h = ANTUNES_2017()
+    # Getting system's model and initial variables 
+    M, K, C, W, V, Wc, Vc, phi, phi_c, Fext, q, qd ,qdd, Fc, h = \
+        ANTUNES_2017(True)
+    
+    Nt, Nn  = Fext.shape
+    Nc      = phi_c.shape[0]
+    Nx      = phi.shape[0]
+    
+    # Data arrays' initialization
+    x, xd, xdd, Fc_phys             = np.zeros((Nt, Nx)), np.zeros((Nt, Nx)), \
+                                        np.zeros((Nt, Nx)), np.zeros((Nt, Nc))
+    x[0], xd[0], xdd[0], Fc_phys[0] = give_phys(phi, phi_c, q, qd, qdd, Fc)
+    
+    # Main simulation loop
+    print('------- Simulation running -------')
+    for i in range(1,Nt):
+        if not (100*i/Nt)%5:
+            print(f'{100*i//Nt} %')
+        q, qd, qdd, Fc                  = UK_step(M, K, C, 
+                                                  Fext[[i]], W, 
+                                                  V, Wc, Vc, q, qd, qdd, h)
+        x[i], xd[i], xdd[i], Fc_phys[i] = give_phys(phi, phi_c, q, qd, qdd, Fc)
+    print('------- Simulation over -------')
+    
+    # Results display
+    disp.set_gui_qt()
+    disp.summary_plot(x, Fc_phys, h)
+    
+'''
+-------------------------------------------------------------
+-------------------------------------------------------------
+'''
 
-Nt, Nn  = Fext.shape
-
-q, qd, qdd  = np.zeros((Nt, Nn)), np.zeros((Nt, Nn)), \
-            np.zeros((Nt, Nn))
-q[0], qd[0], qdd[0] = q0, qd0, qdd0
-
-print('------- Simulation running -------')
-for i in range(1,Nt):
-    if not (100*i/Nt)%5:
-        print(f'{100*i//Nt} %')
-    q[i], qd[i], qdd[i] = UK_step(M, K, C, Fext[i], W, V, q[i-1], 
-                                  qd[i-1], qdd[i-1], h)
-print('------- Simulation over -------')
-
-x, xd, xdd = give_x(phi, q, qd, qdd)
-
-plt.figure(figsize=(15,5))
-plt.plot(np.arange(0,h*4000,h), x[:4000,1])
+if __name__ == "__main__":
+     main()
